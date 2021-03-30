a465838adaf73c85a37eeb8bd8929f778f71e298
Igor Vavilin <igorvv.vn@gmail.com>
2021-03-30 14:00:45 +0300

    12 components Header Navbar Profile

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from "../assets/img/logo_transparent.png"
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src={logo} alt="logo"/>
+            SOCIAL NET
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img className="content__header-img" src="https://wallpapershome.ru/images/pages/pic_h/21486.jpg"
+                     alt=""/>
+            </div>
+            <div>
+                avatar + description
+            </div>
+            <div>
+                my posts
+                <div>
+                    new post
+                </div>
+                <div>
+                    post1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
